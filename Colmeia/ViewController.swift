7a9c7b75fadab374a04e95693655cfdcb74b381e
andreanmasiro <andreanmasiro@live.com>
2017-01-04 13:52:11 -0200

    initial commit

---
+++ Colmeia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Colmeia
+//
+//  Created by André Marques da Silva Rodrigues on 04/01/17.
+//  Copyright © 2017 Vergil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
