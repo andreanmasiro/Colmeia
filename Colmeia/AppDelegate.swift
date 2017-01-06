6cdad208d4999d314aab86f1407f2c41be06148b
andreanmasiro <andreanmasiro@live.com>
2017-01-05 23:45:01 -0200

    removing unneeded configuration from appdelegate

--- Colmeia/AppDelegate.swift
+++ Colmeia/AppDelegate.swift
@@ -17,30 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        
-        Parse.enableLocalDatastore()
-        PFUser.enableAutomaticUser()
-        
-        let defaultACL = PFACL();
-        
-        // If you would like all objects to be private by default, remove this line.
-        defaultACL.getPublicReadAccess = true
-        
-        PFACL.setDefault(defaultACL, withAccessForCurrentUser: true)
-        
-        if application.applicationState != UIApplicationState.background {
-            
-            let preBackgroundPush = !application.responds(to: #selector(getter: UIApplication.backgroundRefreshStatus))
-            let oldPushHandlerOnly = !self.responds(to: #selector(UIApplicationDelegate.application(_:didReceiveRemoteNotification:fetchCompletionHandler:)))
-            var noPushPayload = false;
-            if let options = launchOptions {
-                noPushPayload = options[UIApplicationLaunchOptionsKey.remoteNotification] != nil;
-            }
-            if (preBackgroundPush || oldPushHandlerOnly || noPushPayload) {
-                PFAnalytics.trackAppOpened(launchOptions: launchOptions)
-            }
-        }
-        
         let configuration = ParseClientConfiguration {
             $0.applicationId = "SU0myMIe1AUitLKar0mum8My8RbQ87lEaRjjKDgh"
             $0.clientKey = "Fttez4PLMZOJbNty4lfT8QYcSj28worpNdoYxicO"
@@ -48,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         
         Parse.initialize(with: configuration)
-        
+                
         return true
     }
 
