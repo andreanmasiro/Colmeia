97628b928aa4f2253a1cb61b93da2ff2dbf02403
andreanmasiro <andreanmasiro@live.com>
2017-01-08 13:44:33 -0200

    rating view base template added

---
+++ Colmeia/RatingView.swift
@@ -0,0 +1,15 @@
+//
+//  RatingView.swift
+//  Colmeia
+//
+//  Created by André Marques da Silva Rodrigues on 08/01/17.
+//  Copyright © 2017 Vergil. All rights reserved.
+//
+
+import UIKit
+
+class RatingView: UIView {
+
+    
+
+}
